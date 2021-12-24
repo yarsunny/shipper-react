57808d45f11445295607a05fa4eb45931597a46e
sanath <sanath.yapatla@prasthana.com>
2021-12-24 18:23:24 +0530

    Add Tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      App
+      <h1 className="text-3xl font-bold underline">Shipper</h1>
     </div>
   );
 }
