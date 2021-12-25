17f3f052406ebe457da8789f385e39c313f45b7a
sanath <sanath.yapatla@prasthana.com>
2021-12-25 20:09:03 +0530

    Fix build error caused due to git case sensitive in macos

---
+++ src/features/driver/DriverList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import DriverCard from "./DriverCard";
+
+function DriverList({ drivers }) {
+  if (drivers.length === 0) {
+    return <div className="text-slate-800 p-6 font-bold text-center mt-5">No drivers found</div>;
+  }
+
+  return (
+    <div className="md:overflow-x-auto md:whitespace-nowrap w-full">
+      {drivers.map((driver) => (
+        <DriverCard
+          driver={driver}
+          key={`${driver.name.first}_${driver.name.last}_${driver.dob.date}`}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default React.memo(DriverList);
